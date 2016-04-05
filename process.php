249294a740257c75179d890fdd43794672b69383
Sulaiman Aloraini <sulaimanoraini@yahoo.com>
2016-04-05 23:30:45 +0300

    Added files via upload

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Cookie homework Sulaiman Aloraini</title>
+<title>Cookie HomeWork</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet"
@@ -10,59 +10,61 @@
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script
 	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	<script src="script.js"></script>
+<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-<?php
-$cookie_fname = "fname";
-$cookie_fname_value = $_POST["fname"];
 
-$cookie_lname = "lname";
-$cookie_lname_value = $_POST['lname'];
 
-$cookie_male = "male";
-$cookie_male_value = $_POST['male'];
-
-$cookie_bdate = "bdate";
-$cookie_bdate_value = $_POST['bdate'];
-
-$cookie_coloreye = "color-eye";
-$cookie_coloreye_value = $_POST['color-eye'];
-
-$cookie_sa = "sa";
-$cookie_sa_value = $_POST['sa'];
-
-$cookie_yourimage = "yourimage";
-$cookie_yourimage_value = $_POST['yourimage'];
-
-$cookie_height = "height";
-$cookie_height_value = $_POST['height'];
-
-$cookie_weight = "weight";
-$cookie_weight_value = $_POST['weight'];
-
-$cookie_email = "email";
-$cookie_email_value = $_POST['email'];
-
-$cookie_mobile = "mobile";
-$cookie_mobile_value = $_POST['mobile'];
-
-$cookie_address = "address";
-$cookie_address_value = $_POST['address'];
-
-setcookie($cookie_fname, $cookie_fname_value, time() + (86400 * 300), "/");
-setcookie($cookie_lname, $cookie_lname_value, time() + (86400 * 300), "/");
-setcookie($cookie_male, $cookie_male_value, time() + (86400 * 300), "/");
-setcookie($cookie_bdate, $cookie_bdate_value, time() + (86400 * 300), "/");
-setcookie($cookie_coloreye, $cookie_coloreye_value, time() + (86400 * 300), "/");
-setcookie($cookie_sa, $cookie_sa_value, time() + (86400 * 300), "/");
-setcookie($cookie_yourimage, $cookie_yourimage_value, time() + (86400 * 300), "/");
-setcookie($cookie_height,$cookie_height_value, time() + (86400 * 300), "/");
-setcookie($cookie_weight, $cookie_weight_value, time() + (86400 * 300), "/");
-setcookie($cookie_email, $cookie_email_value, time() + (86400 * 300), "/");
-setcookie($cookie_mobile, $cookie_mobile_value, time() + (86400 * 300), "/");
-setcookie($cookie_address, $cookie_address_value, time() + (86400 * 300), "/");
-?>
+	<?php
+	$cookie_fname = "fname";
+	$cookie_fname_value = $_POST ["fname"];
+	
+	$cookie_lname = "lname";
+	$cookie_lname_value = $_POST ['lname'];
+	
+	$cookie_male = "male";
+	$cookie_male_value = $_POST ['male'];
+	
+	$cookie_bdate = "bdate";
+	$cookie_bdate_value = $_POST ['bdate'];
+	
+	$cookie_coloreye = "color-eye";
+	$cookie_coloreye_value = $_POST ['color-eye'];
+	
+	$cookie_sa = "country";
+	$cookie_sa_value = $_POST ['sa'];
+	
+	$cookie_yourimage = "yourimage";
+	$cookie_yourimage_value = $_POST ['yourimage'];
+	
+	$cookie_height = "height";
+	$cookie_height_value = $_POST ['height'];
+	
+	$cookie_weight = "weight";
+	$cookie_weight_value = $_POST ['weight'];
+	
+	$cookie_email = "email";
+	$cookie_email_value = $_POST ['email'];
+	
+	$cookie_mobile = "mobile";
+	$cookie_mobile_value = $_POST ['mobile'];
+	
+	$cookie_address = "address";
+	$cookie_address_value = $_POST ['address'];
+	
+	setcookie ( $cookie_fname, $cookie_fname_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_lname, $cookie_lname_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_male, $cookie_male_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_bdate, $cookie_bdate_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_coloreye, $cookie_coloreye_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_sa, $cookie_sa_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_yourimage, $cookie_yourimage_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_height, $cookie_height_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_weight, $cookie_weight_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_email, $cookie_email_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_mobile, $cookie_mobile_value, time () + (86400 * 300), "/" );
+	setcookie ( $cookie_address, $cookie_address_value, time () + (86400 * 300), "/" );
+	?>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4"></div>
@@ -74,50 +76,68 @@ setcookie($cookie_address, $cookie_address_value, time() + (86400 * 300), "/");
 			<div class="col-md-4"></div>
 		</div>
 	</div>
-	
-		<div class="container">
+
+	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-			<br><p>
-			<h2>JavaScript cookie</h2>
-			<h4>
+				<br>
+				<h2>JavaScript cookie</h2>
+				<h4>
 					<script type="text/javascript">  
 					document.write("Every Cookie in javascript: " +document.cookie); 
-					document.write("<br><br>Every Cookie in local: " + document.cookie); 
-					</script> 
-					
-					
-			</h4>
+					</script>
+
+
+				</h4>
 				
-			</p>
-			
+				<div>
+					<h3 id="result1"></h3>
+					<h3 id="result2"></h3>
+					<h3 id="result3"></h3>
+					<h3 id="result4"></h3>
+					<h3 id="result5"></h3>
+					<h3 id="result6"></h3>
+					<h3 id="result7"></h3>
+					<h3 id="result8"></h3>
+					<h3 id="result9"></h3>
+				</div>
+				
 			</div>
 			<div class="col-md-4">
-			<br>
-			
-			<p> 
-			<h2>PHP cookie</h2>
-			<?php
-			$cookieArray = array($cookie_fname, $cookie_lname, $cookie_male,
-								$cookie_bdate, $cookie_coloreye, $cookie_sa, $cookie_yourimage,
-								$cookie_height, $cookie_weight, $cookie_email, $cookie_mobile, $cookie_address);
-								
+				<br>
 
-						  echo "<h3>First Name</h3> : ".$_COOKIE[$cookieArray[0]] . "<br>"."<h3>Last Name : </h3>". $_COOKIE[$cookieArray[1]] . "<br>";
-						  echo "<h3>Gender : </h3>".$_COOKIE[$cookieArray[3]] . "<br>"."<h3>Birth Date :</h3> ". $_COOKIE[$cookieArray[4]] . "<br>";
-						  echo "<h3>Country : </h3>".$_COOKIE[$cookieArray[5]] . "<br>"."<h3>yourimage : </h3>" .$_COOKIE[$cookieArray[6]] . "<br>";
-						  echo "<br>"."<h3>height :</h3> ".$_COOKIE[$cookieArray[7]] . "<br>"."<h3>weight : </h3>" .$_COOKIE[$cookieArray[8]] . "<br>";
-						  echo "<h3>email :</h3>".$_COOKIE[$cookieArray[9]] . "<br>"."<h3>mobile : </h3>" .$_COOKIE[$cookieArray[10]] . "<br>";
-						  echo "<h3>Address :</h3>".$_COOKIE[$cookieArray[11]]."<br>";
-		
+				<p>
+				
+				
+				<h2>PHP cookie</h2>
+			<?php
+			$cookieArray = array (
+					$cookie_fname,
+					$cookie_lname,
+					$cookie_male,
+					$cookie_bdate,
+					$cookie_coloreye,
+					$cookie_sa,
+					$cookie_yourimage,
+					$cookie_height,
+					$cookie_weight,
+					$cookie_email,
+					$cookie_mobile,
+					$cookie_address 
+			);
+			
+			echo "<h3>First Name</h3> : " . $_COOKIE [$cookieArray [0]] . "<br>" . "<h3>Last Name : </h3>" . $_COOKIE [$cookieArray [1]] . "<br>";
+			echo "<h3>Gender : </h3>" . $_COOKIE [$cookieArray [3]] . "<br>" . "<h3>Birth Date :</h3> " . $_COOKIE [$cookieArray [4]] . "<br>";
+			echo "<h3>Country : </h3>" . $_COOKIE [$cookieArray [5]] . "<br>" . "<h3>yourimage : </h3>" . $_COOKIE [$cookieArray [6]] . "<br>";
+			echo "<br>" . "<h3>height :</h3> " . $_COOKIE [$cookieArray [7]] . "<br>" . "<h3>weight : </h3>" . $_COOKIE [$cookieArray [8]] . "<br>";
+			echo "<h3>email :</h3>" . $_COOKIE [$cookieArray [9]] . "<br>" . "<h3>mobile : </h3>" . $_COOKIE [$cookieArray [10]] . "<br>";
+			echo "<h3>Address :</h3>" . $_COOKIE [$cookieArray [11]] . "<br>";
+			
 			?>
-			</p>
+			
 			</div>
 			<div class="col-md-4"></div>
 		</div>
 	</div>
-
-
-
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,99 +1,126 @@
-/* function createCookie(newCookie) {
-
-var expDate = new Date();
-
-expDate.setMonth(expDate.getMonth() + 1);    	// Adds 1 month ahead as the expiration date
-
-var cookieVal = document.getElementById(newCookie).value;
-
-// Retrieves cookie value from the node that was past to the function
-
-document.cookie = newCookie + "=" + cookieVal + expDate.toGMTString();    	// Creates the cookie
-
-}
- */
+//check the form is vaild
 function myFunction() {
-	var x, text;
-
-	
-	x = document.getElementById("age").value;
-
-	// If x is Not a Number or less than 19
-	if (isNaN(x) || x < 19) {
-		alert("wrong input")
+	var fname = document.getElementById("fname").value;
+	if(fname == "" || fname == null || !isNaN(fname)){
+		alert("Error in first name");
 		return false;
-	} else {
-	
-			var expDate = new Date();
-
-			expDate.setMonth(expDate.getMonth() + 1);    	// Adds 1 month ahead as the expiration date
-
-			var cookieVal = document.getElementById(x).value;
-
-			// Retrieves cookie value from the node that was past to the function
-
-			document.cookie = x + "=" + cookieVal + expDate.toGMTString();    	// Creates the cookie
-
+	}else {
+		return true;
 	}
 
-}
-
-function locals(){
-	
-	// Check browser support
-	var x = document.getElementById("age").value;
-if (typeof(Storage) !== "undefined") {
-    // Store
-    localStorage.setItem("userAge", x);
-    // Retrieve
-    document.cookie = localStorage.getItem("userAge");
-} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-}
-
-}
-
-function validateFn() {
-    var x = document.forms["frm"]["fname"].value;
-    if (x == null || x == "" || isNaN) {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
-function validateLn() {
-    var x = document.forms["frm"]["lname"].value;
-    if (x == null || x == "" || isNaN) {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
-function validateAddress() {
-    var x = document.forms["frm"]["address"].value;
-    if (x == null || x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
-function validateMobile() {
-    var x = document.forms["frm"]["mobile"].value;
-    if (x == null || x == "" || !isNaN || x.length < 10) {
-        alert("Name must be filled out");
-        return false;
-    } else {
-		alert("done");
+	var lname = document.getElementById("lname").value;
+	if(lname == "" || lname == null || !isNaN(lname)){
+		alert("Error in last name");
+		return false;
+	}else {
+		return true;
 	}
+
+
+	var email = document.getElementById("email").value;
+	if(email == "" || email == null || !isNaN(email)){
+		alert("Error in email");
+		return false;
+	}else {
+		return true;
+	}
+
+	var mobile = document.getElementById("mobile").value;
+	if(mobile == "" || mobile == null || isNaN(mobile) || mobile.length < 10){
+		alert("Error in mobile");
+		return false;
+	}else {
+		return true;
+	}	
+	
+	CookieJavaScript();
+	localS();
+	
 }
 
-function validateEmail() {
-    var x = document.forms["frm"]["email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("Not a valid e-mail address");
-        return false;
-    }
+function CookieJavaScript(){
+
+	// for color 
+
+	var selectedValue = document.getElementById("color-eye").value;
+	if (selectedValue != "Select Color")
+	{
+		document.cookie = "color=" + selectedValue +
+		"; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	}
+
+
+	document.cookie = "fname=" + escape(document.getElementById("fname").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "lname=" + escape(document.getElementById("lname").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "email=" + escape(document.getElementById("email").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "address=" + escape(document.getElementById("address").value)+ "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "mobile=" + escape(document.getElementById("mobile").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "weight=" + escape(document.getElementById("weight").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "height=" + escape(document.getElementById("height").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "ciuntry2=" + escape(document.getElementById("country2").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "country=" + escape(document.getElementById("country").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "bdate=" + escape(document.getElementById("bdate").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+	document.cookie = "male=" + escape(document.getElementById("male").value) + "; "+"expires=Fri, 5 Aug 2016 01:00:00 UTC;";
+
 }
+// localStorage
+function localS(){
+	
+	var fname = document.getElementById("fname").value;
+	var lname= document.getElementById("lname").value;
+	var email = document.getElementById("email").value;
+	var address = document.getElementById("address").value;
+	var mobile= document.getElementById("mobile").value;
+	var weight = document.getElementById("weight").value;
+	var height = document.getElementById("height").value;
+	var country2 = document.getElementById("country2").value;
+	var country = document.getElementById("country").value;
+	var bdate = document.getElementById("bdate").value;
+	var male = document.getElementById("male").value;
+	
+    localStorage.setItem("fname", fname);
+    document.getElementById("result").innerHTML = localStorage.getItem("fname");
+    
+    localStorage.setItem("lname", lname);
+    document.getElementById("result1").innerHTML = localStorage.getItem("lname");
+    
+    localStorage.setItem("email", email);
+    document.getElementById("result2").innerHTML = localStorage.getItem("email");
+    
+    localStorage.setItem("address", address);
+    document.getElementById("result3").innerHTML = localStorage.getItem("address");
+    
+    localStorage.setItem("mobile", mobile);
+    document.getElementById("result4").innerHTML = localStorage.getItem("mobile");
+    
+    localStorage.setItem("weight", weight);
+    document.getElementById("result5").innerHTML = localStorage.getItem("weight");
+    
+    localStorage.setItem("height", height);
+    document.getElementById("result6").innerHTML = localStorage.getItem("height");
+    
+    localStorage.setItem("country2", country2);
+    document.getElementById("result7").innerHTML = localStorage.getItem("country2");
+    
+    localStorage.setItem("bdate", bdate);
+    document.getElementById("result8").innerHTML = localStorage.getItem("bdate");
+    
+    ocalStorage.setItem("male", male);
+    document.getElementById("result9").innerHTML = localStorage.getItem("male");
+    
+}
+
+
+//function setCookie(cname,cvalue) {
+//    var d = new Date();
+//    d.setTime(d.getTime() + (5*24*60*60*1000));
+//    var expires = "expires=" + d.toGMTString();
+//    document.cookie = cname+"="+cvalue+"; "+expires;
+//}
+
+
+
+
+
+
 
